c3a751439f05d43d4374b94f9d21c39986d2f77f
antoinechalifour <antoine.chalifour@gmail.com>
2017-12-30 15:39:11 +0100

    Add weather polling

--- src/scenes/Weather/Weather/index.js
+++ src/scenes/Weather/Weather/index.js
@@ -1,7 +1,32 @@
+import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import PropTypes from 'prop-types'
 import { getWeather } from 'queries/weather'
 import Weather from './Weather'
+
+class WeatherContainer extends Component {
+  static propTypes = {
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+    data: PropTypes.shape({
+      startPolling: PropTypes.func.isRequired,
+      stopPolling: PropTypes.func.isRequired
+    }).isRequired
+  }
+
+  componentDidMount () {
+    console.log('Starting polling...')
+    this.props.data.startPolling(300000)
+  }
+
+  componentWillUnmount () {
+    this.props.data.stopPolling()
+  }
+
+  render () {
+    return <Weather {...this.props} />
+  }
+}
 
 const queryOptions = {
   options: ({ longitude, latitude }) => ({
@@ -9,11 +34,4 @@ const queryOptions = {
   })
 }
 
-const WeatherContainer = graphql(getWeather, queryOptions)(Weather)
-
-WeatherContainer.propTypes = {
-  latitude: PropTypes.number.isRequired,
-  longitude: PropTypes.number.isRequired
-}
-
-export default WeatherContainer
+export default graphql(getWeather, queryOptions)(WeatherContainer)
