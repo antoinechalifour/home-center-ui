0c978ae249e6679ba91a7904327ce7bc3ef904e9
antoinechalifour <antoine.chalifour@gmail.com>
2018-01-15 20:26:52 +0100

    Add new calendar design

--- src/scenes/Calendar/Calendar.js
+++ src/scenes/Calendar/Calendar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import LeftArrow from 'react-icons/lib/md/chevron-left'
-import RightArrow from 'react-icons/lib/md/chevron-right'
+import CalendarIcon from 'react-icons/lib/md/date-range'
+import OrganizerIcon from 'react-icons/lib/md/account-circle'
+import AttendeesIcon from 'react-icons/lib/md/people'
 import gqlLoaderHoc from 'components/GqlLoader'
+import { isSameDay, format as formatDate } from 'util/dates'
 
 export class Calendar extends Component {
   static propTypes = {
@@ -32,175 +34,140 @@ export class Calendar extends Component {
     }).isRequired
   }
 
-  state = {
-    offset: 0
+  _renderEvent = event => {
+    return (
+      <Event>
+        <div>{event.title}</div>
+        <div>
+          <CalendarIcon />
+          {' '}
+          {formatDate(new Date(event.start), 'DD/MM hh:mm')}
+          {' '}
+          -
+          {' '}
+          {formatDate(new Date(event.end), 'DD/MM hh:mm')}
+        </div>
+        <div>
+          <OrganizerIcon /> {event.organizer.name}
+        </div>
+        {event.attendees.length
+          ? <Attendees>
+            <AttendeesIcon />
+            <ul>
+              {event.attendees.map(x => <li>{x.name}</li>)}
+            </ul>
+          </Attendees>
+          : null}
+      </Event>
+    )
   }
 
-  _addDays (date, no) {
-    const newDate = new Date()
-    newDate.setTime(date.getTime() + no * 24 * 60 * 60 * 1000)
+  _renderToday () {
+    const today = new Date()
+    const events = this.props.data.calendar.events.filter(event => {
+      const startDate = new Date(event.start)
+      const endDate = new Date(event.end)
+      if (isSameDay(today, startDate)) {
+        return true
+      } else if (isSameDay(today, endDate)) {
+        return true
+      } else {
+        return today >= startDate && today <= endDate
+      }
+    })
 
-    return newDate
+    return (
+      <div>
+        <Header>Today's events</Header>
+        {events.length
+          ? <ul>
+            {events.map(this._renderEvent)}
+          </ul>
+          : <Empty>Nothing planned for today! Enjoy your day.</Empty>}
+      </div>
+    )
   }
 
-  _getEvents (day, hour) {
-    const date = new Date(day)
-
-    date.setHours(hour)
-    date.setMinutes(30)
-    date.setSeconds(0)
-    date.setMilliseconds(0)
+  _renderUpcomingEvents () {
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    tomorrow.setHours(0)
+    tomorrow.setMinutes(0)
+    tomorrow.setSeconds(0)
+    tomorrow.setMilliseconds(0)
 
     const events = this.props.data.calendar.events.filter(event => {
       const startDate = new Date(event.start)
       const endDate = new Date(event.end)
 
-      return date >= startDate && date <= endDate
+      return startDate >= tomorrow || endDate > tomorrow
     })
 
-    return (
-      <ul>
-        {events.map(x => <li>{x.title}</li>)}
-      </ul>
-    )
-  }
-
-  _isToday (date) {
-    const today = new Date()
-
-    return (
-      date.getDate() === today.getDate() &&
-      date.getMonth() === today.getMonth() &&
-      date.getFullYear() === today.getFullYear()
-    )
-  }
-
-  _isNow (day, hour) {
-    const today = new Date()
-
-    return this._isToday(day) && today.getHours() === hour
+    return events.length
+      ? <div>
+        <Header>Upcoming events</Header>
+        <ul>
+          {events.map(this._renderEvent)}
+        </ul>
+      </div>
+      : null
   }
 
   render () {
-    const date = this._addDays(new Date(), this.state.offset * 7)
-    const days = Array(7).fill(0).map((_, index) => {
-      return this._addDays(date, index)
-    })
-    const hours = Array(24).fill(0).map((_, index) => index)
-
     return (
-      <Container>
-        <CalendarWrapper>
-          <button
-            onClick={() => this.setState({ offset: this.state.offset - 1 })}
-          >
-            <LeftArrow />
-          </button>
-          <Table>
-            <Thead>
-              <Tr>
-                <Th first />
-                {days.map(day => (
-                  <Th active={this._isToday(day)}>
-                    {day.getDate()}/{day.getMonth() + 1}/{day.getFullYear()}
-                  </Th>
-                ))}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {hours.map(hour => (
-                <Tr>
-                  <Td first>{hour}h</Td>
-                  {days.map(day => (
-                    <Td active={this._isNow(day, hour)}>
-                      {this._getEvents(day, hour)}
-                    </Td>
-                  ))}
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-          <button
-            onClick={() => this.setState({ offset: this.state.offset + 1 })}
-          >
-            <RightArrow />
-          </button>
-        </CalendarWrapper>
-      </Container>
+      <div>
+        {this._renderToday()}
+        {this._renderUpcomingEvents()}
+      </div>
     )
   }
 }
 
 export default gqlLoaderHoc(Calendar)
 
-const Container = styled.div`
-  position: relative;
-  height: 100%;
-`
-
-const CalendarWrapper = styled.div`
-  display: flex;
-  position: absolute;
-  top: 12px;
-  right: 12px;
-  bottom: 12px;
-  left: 12px;
-
-  button {
-    cursor: pointer;
-    background: none;
-    outline: none;
-    border: none;
-    color: inherit;
-    font-size: inherit;
-  }
-`
-
-const Table = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-`
-
-const Thead = styled.div`
-  background: rgba(0, 0, 0, .25);
-`
-
-const Tbody = styled.div`
-  height: 100%;
-  flex: 1;
-  overflow-y: auto;
-`
-
-const Tr = styled.div`
-  display: flex;
-
-  :nth-child(even) {
-    background: rgba(0, 0, 0, .1);
-  }
-`
-
-const Td = styled.div`
-  border: 1px solid rgba(255, 255, 255, .15);
-  min-height: 24px;
-  font-size: 10px;
-  word-break: break-all;
+const Header = styled.div`
   text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  ${({ first }) => (first ? `
-    width: 40px;
-    padding: 6px;
-  ` : `
-    padding: 6px 12px;
-    flex: 1;
-  `)}
-  ${({ active }) => active && 'background: rgba(255, 255, 255, .24);'}
+  text-transform: uppercase;
+  color: #fff;
+  padding: 12px;
+  background: ${({ theme }) => theme.colors.primary};
 `
 
-const Th = Td.extend`
-  font-size: 9px;
+const Event = styled.li`
+  padding: 12px;
+
+  > div {
+    display: flex;
+    align-items: center;
+  }
+
+  > div + div {
+    margin-top: 12px;
+  }
+
+  > div:not(:first-child) {
+    padding-left: 8px;
+    opacity: .54;
+    font-size: 90%;
+  }
+
+  svg {
+    opacity: .54;
+    font-size: 150%;
+    margin-right: 12px;
+  }
+  
+  + li {
+    border-top: 1px solid rgba(0, 0, 0, .15);
+  }
+`
+
+const Attendees = styled.div`
+  align-items: flex-start !important;
+`
+
+const Empty = styled.div`
+  padding: 12px;
+  text-align: center;
+  opacity: .54;
 `
--- src/util/dates.js
+++ src/util/dates.js
@@ -11,3 +11,31 @@ export function getDayName (date) {
 
   return days[date.getDay()]
 }
+
+export function isSameDay (d1, d2) {
+  return (
+    d1.getDate() === d2.getDate() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getFullYear() === d2.getFullYear()
+  )
+}
+
+function formatDigits (number) {
+  let formatted = number.toString()
+
+  return number < 10 ? formatted.padStart(2, '0') : formatted
+}
+
+export function format (date, format) {
+  return format
+    .split('YYYY')
+    .join(date.getFullYear())
+    .split('MM')
+    .join(formatDigits(date.getMonth() + 1))
+    .split('DD')
+    .join(formatDigits(date.getDate()))
+    .split('hh')
+    .join(formatDigits(date.getHours()))
+    .split('mm')
+    .join(formatDigits(date.getMinutes()))
+}
