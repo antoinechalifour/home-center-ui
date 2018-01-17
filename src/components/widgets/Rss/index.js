5b62e199c9a728dc2d44b404c2fbea3f336b2228
antoinechalifour <antoine.chalifour@gmail.com>
2018-01-17 19:38:40 +0100

    Redesign rss feed

--- src/components/widgets/Rss/index.js
+++ src/components/widgets/Rss/index.js
@@ -1,38 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import Tabs, * as tabs from 'ui/Tabs'
 import Feed from './Feed'
-import Sources from './Sources'
 
 export default function Rss () {
   return (
-    <Tabs
-      inititalActiveTab='feed'
-      render={({ activeTab, changeTab }) => (
-        <Container>
-          <tabs.Header>
-            <tabs.Tab onClick={() => changeTab('feed')}>News</tabs.Tab>
-            <tabs.Tab onClick={() => changeTab('sources')}>Sources</tabs.Tab>
-          </tabs.Header>
-
-          <Content>
-            {activeTab === 'feed' && <Feed />}
-            {activeTab === 'sources' && <Sources />}
-          </Content>
-        </Container>
-      )}
-    />
+    <div>
+      <Title>Highlights</Title>
+      <Content>
+        <Feed />
+      </Content>
+    </div>
   )
 }
 
-const Container = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
+const Title = styled.div`
+  background: ${({ theme }) => theme.colors.primary};
+  color: #fff;
+  padding: 12px;
+  text-align: center;
+  text-transform: uppercase;
 `
 
 const Content = styled.div`
-  flex: 1;
   padding: 0 12px;
-  overflow-y: auto;
 `
